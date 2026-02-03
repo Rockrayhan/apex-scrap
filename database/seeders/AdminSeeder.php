23d8a7cf13f35ee63836b3e955766ff6238c9cb0
Khayrul Alam <86905281+Rockrayhan@users.noreply.github.com>
2026-02-03 11:22:48 +0600

    Update AdminSeeder.php

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -14,11 +14,6 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        Admin::create([
 
-            'name' => 'Admin',
-            'email' => 'admin@gmail.com',
-            'password' => Hash::make('123456789'),
-        ]);
     }
 }
